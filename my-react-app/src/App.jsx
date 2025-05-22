2ea126b400bfbbf3a02958a6b06547c79b3570dc
fi1ss <ahaho@vk.com>
2025-05-22 11:54:56 +0500

    Theme in context

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState} from 'react';
+// import './App.css'
+import './Themes.css'
+
+const themeContext = createContext();
+
+function App() {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = (() => {
+    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')
+  })
+
+  return (
+      <themeContext.Provider value = {{theme, toggleTheme}}>
+        <body className={theme}>
+          <Toolbar />
+        </body>
+        
+        
+
+      </themeContext.Provider>
+  )
+}
+
+function Toolbar() {
+  return (
+    <ThemeButton />
+  )
+}
+
+function ThemeButton() {
+  const { theme, toggleTheme } = useContext(themeContext);
+
+  return (
+    <button className={theme} onClick={toggleTheme}>Переключить тему</button>
+  )
+
+}
+
+
+
+export default App
